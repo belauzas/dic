53c7ac48f8a477f401dbb24997b20361347f1c70
Rimantas <belauzas@yahoo.com>
2022-11-07 01:22:39 +0200

    Data caching

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
 import dic from '../data/dic.js';
 import { dictionaryInit } from './list.js';
 
-const repoURL = 'https://raw.githubusercontent.com/belauzas/dic/master/package.json';
+const repoURL = 'https://raw.githubusercontent.com/belauzas/dic/master/';
+const packageFile = 'package.json';
+const dataFile = 'data/dic.json';
 
-fetch(repoURL)
-    .then()
+let localDataVersion = '0.0.0';
+let originDataVersion = '0.0.0';
+
+const localVersionKey = 'dic-version';
+const localDataKey = 'dic-content';
+const localVersion = localStorage.getItem(localVersionKey);
+
+if (localVersion) {
+    localDataVersion = localVersion;
+}
+
+await fetch(repoURL + packageFile)
+    .then(data => data.json())
+    .then(data => {
+        originDataVersion = data.version;
+    })
     .catch((e) => {
         console.log(e);
     })
 
+if (localDataVersion !== originDataVersion) {
+    await fetch(repoURL + dataFile)
+        .then(data => data.json())
+        .then(data => {
+            localStorage.setItem(localVersionKey, originDataVersion);
+            localStorage.setItem(localDataKey, JSON.stringify(data));
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+}
 
-dictionaryInit(dic);
+const localDataJSON = localStorage.getItem(localDataKey);
+try {
+    const localData = JSON.parse(localDataJSON);
+    dictionaryInit(localData);
+} catch (error) {
+    console.log(error);
+}
